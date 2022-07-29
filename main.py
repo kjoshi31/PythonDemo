085f8dac8d50438b3d2e56ee6cd7248ef53a9f00
Karan Joshi <kjoshi@fvfhv0hrq6lw.lan>
2022-07-29 16:57:10 -0400

    reformatted file and get_user method

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def hello_world():
     return "HELLO, World!!"
 
+
 @app.route('/another')
 def another():
     return "another route!"
-    
-def json_return():
+
+
+def json_list():
     return {0: ['hi', 'bye', 'no']}
+
+
+@app.route('/user')
+def get_user():
+    return {'id': 1}
